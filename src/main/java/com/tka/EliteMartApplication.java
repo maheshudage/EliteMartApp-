f82212bb1b02f96948063924b5e47b6f6df53c4c
maheshudage <udagemahesh89@gmail.com>
2026-01-17 11:33:52 +0530

    Update EliteMartApplication.java

--- src/main/java/com/tka/EliteMartApplication.java
+++ src/main/java/com/tka/EliteMartApplication.java
@@ -3,6 +3,7 @@ package com.tka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication(scanBasePackages = "com.tka")
 @SpringBootApplication
 public class EliteMartApplication {
 
