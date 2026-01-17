e5dafe6b27c3b0332da7d9020cb10acdd7743878
maheshudage <udagemahesh89@gmail.com>
2026-01-17 11:42:56 +0530

    Update HomeController.java

--- src/main/java/com/tka/controller/HomeController.java
+++ src/main/java/com/tka/controller/HomeController.java
@@ -1,21 +1,16 @@
 package com.tka.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tka.service.ProductService;
 
-@Controller
+@RestController
 public class HomeController {
 
-	@Autowired
-	private ProductService productService;
-
-	@GetMapping("/")
-	public String getIndexPage(Model model) {
-		model.addAttribute("products", productService.getAllProducts());
-		return "index";
-	}
+    @GetMapping("/")
+    public String home() {
+        return "OK";
+    }
 }
+
